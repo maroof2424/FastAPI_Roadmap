8e8f489588be41eb3cad7d1ea877ac665d4e00ce
M.Maroof <maroof96965@gmail.com>
2025-08-13 18:49:21 +0500

    week2 day1

--- Week02_CRUD_Pydantic_Routers/app/routers/posts.py
+++ Week02_CRUD_Pydantic_Routers/app/routers/posts.py
@@ -1,1 +1,17 @@
-# posts.py
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/posts",
+    tags=["Posts"]
+)
+
+post_data = [
+    {"id":1,"title":"First Post","content":"This is my first blog post"},
+    {"id":2,"title":"Second Post","content":"Learning FastAPI is fun!"},
+]
+
+@router.get("/")
+def get_posts():
+    return {"status":"success","data":post_data}
+
+    
--- Week02_CRUD_Pydantic_Routers/main.py
+++ Week02_CRUD_Pydantic_Routers/main.py
@@ -1,1 +1,10 @@
-# main.py
+from fastapi import FastAPI
+from app.routers import posts
+
+app = FastAPI(title="Blog API - Week 2 CRUD")
+
+app.include_router(posts.router)
+
+@app.get("/")
+def root():
+    return {"message":"Welcome to Blog API."}
