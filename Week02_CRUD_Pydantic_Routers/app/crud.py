9bc6710093e19829209e597163dffacf2f5adc4e
maroof2424 <maroof96965@gmail.com>
2025-08-20 10:26:24 +0500

    day7

--- Week02_CRUD_Pydantic_Routers/app/crud.py
+++ Week02_CRUD_Pydantic_Routers/app/crud.py
@@ -1,31 +1,24 @@
 from app import schemas
 
-post_data = [
-    {"id": 1, "title": "First Post", "content": "This is my first blog post", "author": "Maroof"},
-    {"id": 2, "title": "Second Post", "content": "Learning FastAPI is fun!", "author": "Maroof"},
-]
+post_data = []
 
-def get_post():
+def get_posts():
     return post_data
 
-def create_post(post:schemas.PostCreate):
-    new_id = len(post_data)+1
-    new_post = {'id':new_id,**post.dict()}
+def get_post_by_id(post_id: int):
+    return next((post for post in post_data if post["id"] == post_id), None)
+
+def create_post(post: schemas.PostCreate):
+    new_id = len(post_data) + 1
+    new_post = {"id": new_id, **post.dict()}
     post_data.append(new_post)
     return new_post
 
-def get_post_by_id(post_id:int):
-    for post in post_data:
-        if post["id"] == post_id:
-            return post
-    return None
-    
-
 def update_post(post_id: int, post: schemas.PostCreate):
-    existing_Post = get_post_by_id(post_id)
-    if existing_Post:
-        existing_Post.update(post.dict())
-        return existing_Post
+    existing_post = get_post_by_id(post_id)
+    if existing_post:
+        existing_post.update(post.dict())
+        return existing_post
     return None
 
 def delete_post(post_id: int):
@@ -35,9 +28,8 @@ def delete_post(post_id: int):
         return True
     return False
 
-def search_posts(keyword:str):
-    results = []
-    for post in post_data:
-        if keyword.lower() in post["title"].lower() or keyword.lower() in post["content"].lower():
-            results.append(post)
-    return results
+def search_posts(keyword: str):
+    return [
+        post for post in post_data
+        if keyword.lower() in post["title"].lower() or keyword.lower() in post["content"].lower()
+    ]
--- Week02_CRUD_Pydantic_Routers/app/schemas.py
+++ Week02_CRUD_Pydantic_Routers/app/schemas.py
@@ -2,12 +2,16 @@ from pydantic import BaseModel
 from typing import Optional
 
 class PostCreate(BaseModel):
-    title : str
-    content : str
-    author : Optional[str] = "Anonymos"
+    title: str
+    content: str
+    author: Optional[str] = "Anonymous"
 
 class PostOut(BaseModel):
     id: int
     title: str
     content: str
-    author:str
+    author: str
+
+class Message(BaseModel):
+    status: str
+    message: str
