70c3e887126c4a13fb57e9a8ec6d62eef5d5067c
maroof2424 <maroof96965@gmail.com>
2025-08-24 09:31:21 +0500

    day5

--- Week03_Database_SQLAlchemy/app/routers/posts.py
+++ Week03_Database_SQLAlchemy/app/routers/posts.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app import models,schemas
+from app import schemas, crud
 from app.database import get_db
 
 router = APIRouter(
@@ -8,14 +8,11 @@ router = APIRouter(
     tags=["Posts"]
 )
 
-@router.get("/",response_model=list[schemas.PostOut])
-def get_posts(db: Session = Depends(get_db)):
-    return db.query(models.Post).all()
+@router.post("/", response_model=schemas.PostOut)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    return crud.create_post(db, post)
 
-@router.post("/",response_model=schemas.PostOut)
-def create_post(post:schemas.PostCreate,db:Session=Depends(get_db)):
-    new_post = models.Post(title=post.title,content=post.content,author_id = post.author_id)
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
+@router.get("/", response_model=list[schemas.PostOut])
+def read_posts(db: Session = Depends(get_db)):
+    return crud.get_posts(db)
+
--- Week03_Database_SQLAlchemy/app/routers/users.py
+++ Week03_Database_SQLAlchemy/app/routers/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app import models, schemas
+from app import schemas, crud
 from app.database import get_db
 
 router = APIRouter(
@@ -8,21 +8,14 @@ router = APIRouter(
     tags=["Users"]
 )
 
-
-@router.get("/",response_model=list[schemas.UserOut])
-def get_users(db:Session=Depends(get_db)):
-    return db.query(models.User).all()
-
+@router.post("/", response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db, user)
 
 @router.get("/{user_id}", response_model=schemas.UserOut)
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == user_id).first()
-    return user
-    
-@router.post("/",response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate,db:Session=Depends(get_db)):
-    new_user = models.User(username=user.username,email=user.email)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+    return crud.get_user(db, user_id)
+
+@router.get("/", response_model=list[schemas.UserOut])
+def read_users(db: Session = Depends(get_db)):
+    return crud.get_users(db)
