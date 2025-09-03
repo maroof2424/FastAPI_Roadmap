c73152d7be5f0ad33e771d3f9cc62cf9b3f7203e
maroof2424 <maroof96965@gmail.com>
2025-09-03 16:37:15 +0500

    week5 day1

--- Week05_Async_BackgroundTasks/app/endpoints.py
+++ Week05_Async_BackgroundTasks/app/endpoints.py
@@ -1,1 +1,15 @@
-# endpoints.py
+from fastapi import APIRouter
+import asyncio
+
+router = APIRouter()
+
+@router.get("/sync")
+def sync_endpoint():
+    import time
+    time.sleep(3)
+    return {"message":"This was slow (sync)"}
+
+@router.get("/async")
+async def async_endpoint():
+    await asyncio.sleep(3)
+    return {"message":"This is fast (async,non-blocking)"}
--- Week05_Async_BackgroundTasks/app/main.py
+++ Week05_Async_BackgroundTasks/app/main.py
@@ -1,1 +1,6 @@
-# main.py
+from fastapi import FastAPI
+from .endpoints import router as async_router
+
+app = FastAPI()
+
+app.include_router(async_router)
