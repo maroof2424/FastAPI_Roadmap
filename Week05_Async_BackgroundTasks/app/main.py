555f53788578bc94f9516331d74ae731780173c9
maroof2424 <maroof96965@gmail.com>
2025-09-04 16:38:52 +0500

    Day2

--- Week05_Async_BackgroundTasks/app/main.py
+++ Week05_Async_BackgroundTasks/app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from .endpoints import router as async_router
+from . import engine, Base
+from . import demo, models
 
-app = FastAPI()
+app = FastAPI(title="Async FastAPI + SQLite")
 
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+# ✅ Add root route
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Async FastAPI + SQLite 🚀"}
+
+app.include_router(demo.router)
 app.include_router(async_router)
